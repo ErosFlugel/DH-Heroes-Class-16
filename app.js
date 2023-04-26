13144fcc01914e078140c7a523459204ff3b2d0f
ErosFlugel <erosflugel18@gmail.com>
2023-04-26 15:29:28 -0500

    added the images, connected all the responses for the html files on the server, completed the html content for each html page

--- app.js
+++ app.js
@@ -19,3 +19,27 @@ app.use(express.static(path.resolve(__dirname, "./public")));
 
 //Running the server
 app.listen(3030, () => console.log("Servidor corriendo en el puerto 3030"));
+
+//Page => Home
+app.get("/", (req, res) => res.sendFile(path.resolve(__dirname, "./views/index.html")));
+
+// Page => /Turing
+app.get("/turing", (req, res) => res.sendFile(path.resolve(__dirname, "./views/turing.html")));
+
+// Page => /Lovelace
+app.get("/lovelace", (req, res) => res.sendFile(path.resolve(__dirname, "./views/lovelace.html")));
+
+// Page => /Hopper
+app.get("/hopper", (req, res) => res.sendFile(path.resolve(__dirname, "./views/hopper.html")));
+
+// Page => /Hamilton
+app.get("/hamilton", (req, res) => res.sendFile(path.resolve(__dirname, "./views/hamilton.html")));
+
+// Page => /Clarke
+app.get("/clarke", (req, res) => res.sendFile(path.resolve(__dirname, "./views/clarke.html")));
+
+// Page => /Sir-Tim-Berners-Lee
+app.get("/berners-lee", (req, res) => res.sendFile(path.resolve(__dirname, "./views/berners-lee.html")));
+
+// Page => /Babbage
+app.get("/babbage", (req, res) => res.sendFile(path.resolve(__dirname, "./views/Babbage.html")));
